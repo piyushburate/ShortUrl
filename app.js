ebfbb3acd9a892b0441a7068463deaa94ff0a261
PiyushBurate <buratepiyush@gmail.com>
2023-07-03 01:57:43 +0530

    Add url redirecting and login signup page designs

--- app.js
+++ app.js
@@ -10,19 +10,35 @@ let port = 8081
 // Set Static folder
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")))
 
-// Database requests
-
-app.get("/api/db/:table", (req, res)=>{
-    var query = `SELECT * FROM ${req.params.table}`
-    database.query(query, (err, rows)=>{
-        if (err) throw err
-        res.send(rows)
-    })
+// Set Entry Points
+app.get("/user/:id", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "src", "index.html"))
 })
 
-// Set Entry Point
-app.get("/*", (req, res) => {
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", "index.html"))
+})
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "src", "index.html"))
+})
+
+app.get("/*", (req, res, next) => {
+    next()
+})
+
+app.get("/:link_code", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "src", "index.html"))
+    var query = `SELECT link FROM links WHERE code = '${req.params.link_code}'`
+    database.query(query, (err, rows)=>{
+        if (err || rows.length == 0) {
+            // console.log(err);
+            res.send("404 NOT FOUND")
+        } else {
+            res.redirect(rows[0].link)
+            res.send(rows[0].link)
+        }
+    })
 })
 
 // Start Server
