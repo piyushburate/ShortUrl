3cc93e9d8a1d8bd80ef0643732dfaa6a3d6ab0d3
PiyushBurate <buratepiyush@gmail.com>
2023-07-03 10:48:12 +0530

    Solved Error - Header Not Send

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const database = require("./mysql_db_connection")
 
 // Variables
 let app = express()
-let port = 8081
+let port = 3000
 
 // Set Static folder
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")))
@@ -23,23 +23,25 @@ app.get("/signup", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", "index.html"))
 })
 
-app.get("/*", (req, res, next) => {
-    next()
+app.use("/", (req, res) => {
+    // res.send("404 Not Found")
+    res.redirect("/login")
 })
 
-app.get("/:link_code", (req, res) => {
-    // res.sendFile(path.resolve(__dirname, "src", "index.html"))
+app.get("/:link_code", (req, res, next) => {
+    console.log("next");
     var query = `SELECT link FROM links WHERE code = '${req.params.link_code}'`
-    database.query(query, (err, rows)=>{
+    database.query(query, (err, rows) => {
         if (err || rows.length == 0) {
             // console.log(err);
             res.send("404 NOT FOUND")
         } else {
             res.redirect(rows[0].link)
-            res.send(rows[0].link)
+            // res.send(rows[0].link)
         }
     })
 })
+
 
 // Start Server
 app.listen(port, () => {
