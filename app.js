d607025b30a3f65f3d38f515e31c940ae50b7ba1
PiyushBurate <buratepiyush@gmail.com>
2023-07-05 23:11:19 +0530

    Retrieving and displaying data to user links section

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ let port = 3000
 // Set Static folder
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")))
 
+// Database API Points
+app.get("/api/db/:uid/links", (req, res) => {
+    console.log("db");
+    var query = `SELECT id, code, link, DATE_FORMAT(datetime, '%b %e, %Y') AS 'datetime', clicks, link_active FROM links WHERE uid = ${req.params.uid}`
+    database.query(query, (err, rows) => {
+        if (err || rows.length == 0) {
+            res.send("404 NOT FOUND")
+        } else {
+            res.send(rows)
+        }
+    })
+})
+app.get("/api/db/:uid/links/:id", (req, res) => {
+    console.log("db2");
+    var query = `SELECT title, code, link, DATE_FORMAT(datetime, '%M %e, %Y %l:%i %p') AS 'datetime', clicks, link_active FROM links WHERE uid = ${req.params.uid} AND id = ${req.params.id}`
+    database.query(query, (err, rows) => {
+        if (err || rows.length == 0) {
+            res.send("404 NOT FOUND")
+        } else {
+            res.send(rows)
+        }
+    })
+})
+
 // Set Entry Points
 app.get("/user/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", "index.html"))
@@ -23,25 +47,28 @@ app.get("/signup", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", "index.html"))
 })
 
-app.use("/", (req, res) => {
-    // res.send("404 Not Found")
-    res.redirect("/login")
-})
 
-app.get("/:link_code", (req, res, next) => {
+app.get("/:link_code", (req, res) => {
     console.log("next");
-    var query = `SELECT link FROM links WHERE code = '${req.params.link_code}'`
+    var query = `SELECT link FROM links WHERE code = '${req.params.link_code}' AND link_active = TRUE`
     database.query(query, (err, rows) => {
         if (err || rows.length == 0) {
-            // console.log(err);
-            res.send("404 NOT FOUND")
+            res.send("404 NOT FOUND <script>console.log(" + err + ")</script>")
         } else {
-            res.redirect(rows[0].link)
-            // res.send(rows[0].link)
+            database.query("UPDATE links SET clicks = clicks + 1", (err) => {
+                if (err) {
+                    res.send("Error <script>console.log(" + err + ")</script>")
+                } else {
+                    res.redirect(rows[0].link)
+                }
+            })
         }
     })
 })
 
+app.use("/", (req, res) => {
+    res.redirect("/login")
+})
 
 // Start Server
 app.listen(port, () => {
