31e98d9f37c39f41c43eb7daa3ce04672b582bdf
PiyushBurate <buratepiyush@gmail.com>
2023-07-07 02:02:03 +0530

    Added Login and Signup System

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // Requires
 const express = require("express")
+const session = require("express-session")
 const path = require("path")
 const database = require("./mysql_db_connection")
 
 // Variables
 let app = express()
 let port = 3000
+let error404 = "404 Not Found"
 
-// Set Static folder
+// Set use
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}))
 
-// Database API Points
+// API GET requests
 app.get("/api/db/:uid/links", (req, res) => {
-    console.log("db");
+    if (!req.session.loggedin) throw "Error";
     var query = `SELECT id, code, link, DATE_FORMAT(datetime, '%b %e, %Y') AS 'datetime', clicks, link_active FROM links WHERE uid = ${req.params.uid}`
     database.query(query, (err, rows) => {
         if (err || rows.length == 0) {
@@ -23,7 +32,7 @@ app.get("/api/db/:uid/links", (req, res) => {
     })
 })
 app.get("/api/db/:uid/links/:id", (req, res) => {
-    console.log("db2");
+    if (!req.session.loggedin) throw "Error";
     var query = `SELECT title, code, link, DATE_FORMAT(datetime, '%M %e, %Y %l:%i %p') AS 'datetime', clicks, link_active FROM links WHERE uid = ${req.params.uid} AND id = ${req.params.id}`
     database.query(query, (err, rows) => {
         if (err || rows.length == 0) {
@@ -34,30 +43,66 @@ app.get("/api/db/:uid/links/:id", (req, res) => {
     })
 })
 
-// Set Entry Points
+app.get("/api/session/data", (req, res) => {
+    if (req.session.loggedin == true) {
+        var data = {
+            status: "ok",
+            username: req.session.username,
+            email: req.session.email,
+            uid: req.session.uid,
+        }
+        res.send(data)
+    } else {
+        var data = {
+            status: "error",
+            error: "Loggin required to get session data."
+        }
+        res.send(data)
+    }
+
+})
+
+// GET requests
 app.get("/user/*", (req, res) => {
+    if (!req.session.loggedin) {
+        res.redirect("/login")
+        res.end()
+    } else if (req.url.split("/", 3)[2] != req.session.username) throw error404
     res.sendFile(path.resolve(__dirname, "src", "index.html"))
 })
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "src", "index.html"))
+    if (req.session.loggedin == true) {
+        res.redirect("/user/" + req.session.username)
+    } else {
+        res.sendFile(path.resolve(__dirname, "src", "index.html"))
+    }
 })
 
 app.get("/signup", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "src", "index.html"))
+    if (req.session.loggedin == true) {
+        res.redirect("/user/" + req.session.username)
+    } else {
+        res.sendFile(path.resolve(__dirname, "src", "index.html"))
+    }
 })
 
+app.get("/logout", (req, res) => {
+    if (req.session.loggedin == true) {
+        req.session.destroy()
+    }
+    res.redirect("/login")
+})
 
 app.get("/:link_code", (req, res) => {
-    console.log("next");
     var query = `SELECT link FROM links WHERE code = '${req.params.link_code}' AND link_active = TRUE`
     database.query(query, (err, rows) => {
         if (err || rows.length == 0) {
             res.send("404 NOT FOUND <script>console.log(" + err + ")</script>")
         } else {
-            database.query("UPDATE links SET clicks = clicks + 1", (err) => {
-                if (err) {
-                    res.send("Error <script>console.log(" + err + ")</script>")
+            database.query("UPDATE links SET clicks = clicks + 1", (err2, rows2) => {
+                if (err2) {
+                    res.send("Error <script>console.log(" + err2 + ")</script>")
                 } else {
                     res.redirect(rows[0].link)
                 }
@@ -66,8 +111,80 @@ app.get("/:link_code", (req, res) => {
     })
 })
 
+// POST requests
+app.post("/authLogin", (req, res) => {
+    var username = req.body.username
+    var password = req.body.password
+    // console.log(req.body);
+    var query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`
+    database.query(query, (err, rows) => {
+        if (err) {
+            res.send({
+                status: "error",
+                error: err
+            })
+        } else if (rows.length == 0) {
+            res.send({
+                status: "error",
+                error: "Invalid Username or Password!"
+            })
+        } else {
+            req.session.loggedin = true
+            req.session.username = rows[0].username
+            req.session.email = rows[0].email
+            req.session.uid = rows[0].uid
+            res.send({
+                status: "ok",
+                result: "Log in successful!"
+            })
+        }
+    })
+})
+
+app.post("/authSignup", (req, res) => {
+    // console.log("signup");
+    const { name, email, username, password } = req.body
+    // console.log(username);
+    var query = `SELECT * FROM users WHERE username = '${username}'`
+    database.query(query, (err, rows) => {
+        if (err) {
+            res.send({
+                status: "error",
+                error: err
+            })
+        } else if (rows.length > 0) {
+            res.send({
+                status: "error",
+                error: "Username already taken!"
+            })
+        } else {
+            var query2 = `INSERT INTO users (username, name, email, password) VALUES ('${username}', '${name}', '${email}', '${password}')`
+            database.query(query2, (err2, rows2) => {
+                if (err2) {
+                    res.send({
+                        status: "error",
+                        error: err2
+                    })
+                } else {
+                    res.send({
+                        status: "ok",
+                        username: username,
+                        password: password,
+                        result: "Sign up successful!"
+                    })
+                }
+            })
+        }
+    })
+})
+
+// Application Entry Point
 app.use("/", (req, res) => {
-    res.redirect("/login")
+    if (req.session.loggedin == true) {
+        res.redirect("/user/" + req.session.username)
+    } else {
+        res.redirect("/login")
+    }
 })
 
 // Start Server
--- mysql_db_connection.js
+++ mysql_db_connection.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql")
 
+// var database = mysql.createConnection({
+//     host: "localhost",
+//     database: "shorturl",
+//     user: "root",
+//     password: ""
+// })
 var database = mysql.createConnection({
-    host: "localhost",
-    database: "shorturl",
-    user: "root",
-    password: ""
+    host: "sql6.freesqldatabase.com",
+    database: "sql6630821",
+    user: "sql6630821",
+    password: "hy4wPCdlVg"
 })
 
 database.connect(function (err) {
